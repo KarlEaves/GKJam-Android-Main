e42fd08342780b4242d6c5f3752aa7f420de380d
KarlEaves <karleeaves@gmail.com>
2018-04-04 12:11:46 -0600

    Scales page

--- app/src/main/java/com/karl/gux/gkjam/Activities/Fragments/ItemTwoFragment.java
+++ app/src/main/java/com/karl/gux/gkjam/Activities/Fragments/ItemTwoFragment.java
@@ -14,7 +14,9 @@ import com.karl.gux.gkjam.Classes.Scales;
 import com.karl.gux.gkjam.R;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ItemTwoFragment extends Fragment {
 
@@ -74,22 +76,16 @@ public class ItemTwoFragment extends Fragment {
         toggle_button_g_major= getView().findViewById(R.id.GMajor);
         toggle_button_g_sharp_major= getView().findViewById(R.id.GSharpMajor);
 
-
         find_scale.setOnClickListener(new View.OnClickListener()   {
             public void onClick(View v)  {
-                try {
-                    findScale();
-                } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
+
             }
         });
     }
 
 
 
-    public String[] chordsToNote(String chord)
+    public List<String> chordsToNote(String chord)
     {
         List<String> notes_in_chord=new ArrayList<>();
 
@@ -107,44 +103,147 @@ public class ItemTwoFragment extends Fragment {
             root = root+1;
         }
 
+        //======ADD THE ROOT NOTE TO RETURN STRING===============
         notes_in_chord.add(scales.music_notes[root]);
 
-        //if the chord is minor, push the minor 3rd
-        if (chord.charAt(1)=='m' || chord.charAt(2) == 'm')
-        {
-            notes_in_chord.add(scales.music_notes[(root+3)%12]);
-        }
-        //else push major 3rd
-        else
+
+
+        //====IF CHORD IS MAJOR, ADD MAJOR 3rd & 5th===========
+        if (chord.toLowerCase().contains("major"))
         {
             notes_in_chord.add(scales.music_notes[(root+4)%12]);
+            notes_in_chord.add(scales.music_notes[(root+7)%12]);
         }
 
-        notes_in_chord.add(scales.music_notes[(root+7)%12]);
-
-        if (chord.includes("Maj7") || chord.includes("maj7") || chord.includes("M7"))
+        //===IF CHORD IS MINOR, ADD MINOR 3rd & 5th
+        else if (chord.toLowerCase().contains("minor"))
         {
-            notes_in_chord.push(notes[(root+11)%12]);
+            notes_in_chord.add(scales.music_notes[(root+3)%12]);
+            notes_in_chord.add(scales.music_notes[(root+7)%12]);
         }
-        else if (chord.includes("7"))
+
+        //+========IF CHORD IS MAJ7, ADD MAJOR 3RD, 5TH, AND MAJ7
+        else if (chord.toLowerCase().contains("maj7"))
         {
-            notes_in_chord.push(notes[(root+10)%12]);
+            notes_in_chord.add(scales.music_notes[(root+4)%12]);
+            notes_in_chord.add(scales.music_notes[(root+7)%12]);
+            notes_in_chord.add(scales.music_notes[(root+11)%12]);
         }
 
-        //-------------------------print the notes to screen that are in the chords selected-----------------------------
+        //=======IF CHORD IS M7, ADD MINOR 3RD, 5TH, MINOR 7TH=======
+        else if (chord.toLowerCase().contains("m7"))
+        {
+            notes_in_chord.add(scales.music_notes[(root+3)%12]);
+            notes_in_chord.add(scales.music_notes[(root+7)%12]);
+            notes_in_chord.add(scales.music_notes[(root+10)%12]);
+        }
 
-        var para = document.createElement("P");
-        para.appendChild(document.createTextNode(chord +" contains the following notes: "));
-        for (let i = 0;i<notes_in_chord.length; i++)
-        {                     // Create a <p> node
-            var t = document.createTextNode(notes_in_chord[i]+ "  ");      // Create a text node
-            para.appendChild(t);                 // Append the text to <p>
-            document.getElementById("notesInChordsSelected").appendChild(para);
+        //=======IF CHORD IS M7, ADD MINOR 3RD, 5TH, MAJOR 7TH=======
+        else if (chord.toLowerCase().contains("mM7"))
+        {
+            notes_in_chord.add(scales.music_notes[(root+3)%12]);
+            notes_in_chord.add(scales.music_notes[(root+7)%12]);
+            notes_in_chord.add(scales.music_notes[(root+11)%12]);
+        }
+
+        //+========IF CHORD IS MAJ7, ADD MAJOR 3RD, 5TH, AND Min7
+        else if (chord.toLowerCase().contains("7"))
+        {
+            notes_in_chord.add(scales.music_notes[(root+4)%12]);
+            notes_in_chord.add(scales.music_notes[(root+7)%12]);
+            notes_in_chord.add(scales.music_notes[(root+10)%12]);
         }
         return notes_in_chord;
     }
-    public void findScale()
+
+
+    //===============Loops through scales to see if they contain subset of notes to find. Returns all scales that contains matching notes
+    public List<String> findScale(List<String> chords_to_find)
     {
 
+        //=========TURN ALL CHORDS PASSED IN INTO NOTES================================================
+        Set<String> notes_to_find = new HashSet<>();
+        List<String> notes_in_chord;
+
+
+        //loop through input (buttons that were clicked containing chords)
+        for (int i = 0; i<chords_to_find.size();i++)
+            {
+            //turn each chord into notes
+            notes_in_chord = chordsToNote(chords_to_find.get(i));
+            //loop through returned notes and pushes to all notes list
+            for (int j = 0; j < notes_in_chord.size(); j++)
+            {
+                notes_to_find.add(notes_in_chord.get(j));
+            }
+        }
+
+        //==============================================================================
+        //
+        //=======loop through all scales, check if they contain subset of notes in parameter, add to list if they do====================
+        List<String> containing_scales = new ArrayList<>();
+        for (int i = 0;i <scales.major_scales.size();i++)
+        {
+            if (scales.major_scales.get(i).containsAll(notes_to_find))
+            {
+                containing_scales.add(scales.major_scales.get(i).get(0)+" major (ionian)");
+                containing_scales.add(scales.dorian_scales.get((i+2)%12).get(1)+ " dorian");
+                containing_scales.add(scales.phrygian_scales.get((i+4)%12).get(2) + " phrygian");
+                containing_scales.add(scales.lydian_scales.get((i+5)%12).get(3)+ " lydian");
+                containing_scales.add(scales.mixolydian_scales.get((i+7)%12).get(4)+ " mixolydian");
+                containing_scales.add(scales.locrian_scales.get((i+11)%12).get(6)+ " locrian");
+            }
+
+            if (scales.minor_scales.get(i).containsAll(notes_to_find))
+            {
+                containing_scales.add(scales.minor_scales.get(i).get(0)+" minor (aeolian)");
+            }
+
+            if (scales.minor_pentatonic_scales.get(i).containsAll(notes_to_find))
+                {
+                containing_scales.add(scales.minor_pentatonic_scales.get(i).get(0)+" minor pentatonic");
+            }
+
+            if (scales.major_pentatonic_scales.get(i).containsAll(notes_to_find))
+            {
+                containing_scales.add(scales.major_pentatonic_scales.get(i).get(0)+" major pentatonic");
+            }
+
+            if (scales.minor_melodic_scales.get(i).containsAll(notes_to_find))
+            {
+                containing_scales.add(scales.minor_melodic_scales.get(i).get(0)+" melodic minor");
+            }
+
+            if (scales.minor_harmonic_scales.get(i).containsAll(notes_to_find))
+            {
+                containing_scales.add(scales.minor_harmonic_scales.get(i).get(0)+" harmonic minor");
+            }
+
+            if (scales.major_blues_scales.get(i).containsAll(notes_to_find))
+            {
+                containing_scales.add(scales.major_blues_scales.get(i).get(0)+" blues major");
+            }
+
+            if (scales.minor_blues_scales.get(i).containsAll(notes_to_find))
+            {
+                containing_scales.add(scales.minor_blues_scales.get(i).get(0)+" blues minor");
+            }
+
+            if (scales.major_bebop_scales.get(i).containsAll(notes_to_find))
+            {
+                containing_scales.add(scales.major_bebop_scales.get(i).get(0)+" bebop major");
+            }
+
+            if (scales.minor_bebop_scales.get(i).containsAll(notes_to_find))
+            {
+                containing_scales.add(scales.minor_bebop_scales.get(i).get(0)+" bebop minor");
+            }
+        }
+
+        return containing_scales;
     }
+
+
+
+
 }
