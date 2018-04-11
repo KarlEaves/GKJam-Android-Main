1452b217cf10e119e8ce82a537d0abc45eb76350
guxtavito <guxtavito@gmail.com>
2018-04-11 08:57:24 -0600

    Made some chnages

--- app/src/main/java/com/karl/gux/gkjam/Activities/ScalesTextDialogActivity.java
+++ app/src/main/java/com/karl/gux/gkjam/Activities/ScalesTextDialogActivity.java
@@ -1,13 +1,12 @@
 package com.karl.gux.gkjam.Activities;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
 import com.karl.gux.gkjam.R;
 
-public class ScalesTextDialogActivity extends Activity {
+public class ScalesTextDialogActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +14,12 @@ public class ScalesTextDialogActivity extends Activity {
         //Remove title bar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_scales_text_dialog);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
